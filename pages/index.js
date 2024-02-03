2bf10772e4c8e5bcd229077b0008e4055ec83145
LJiaqingNick <gmziue@gmail.com>
2024-02-02 21:14:29 -0800

    integrate with chakra ui

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
+import { Container, Box, Heading } from "@chakra-ui/react"
+
 const Page = () => {
-  return <div>Hello</div>;
+  return(
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I'm a full-stack developer based in Vancouver.
+      </Box>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Nick Liu
+          </Heading>
+          <p> Fullstack Developer / Bodybuilder / Reader </p>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default Page;
