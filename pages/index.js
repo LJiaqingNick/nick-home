ae33577df43871c9a94a8292e19f3a13c5033184
LJiaqingNick <gmziue@gmail.com>
2024-02-11 13:09:12 -0800

    add static profile photo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
-import { Container, Box, Heading } from '@chakra-ui/react';
+import {
+  Container,
+  Box,
+  Heading,
+  Image,
+  useColorModeValue,
+} from '@chakra-ui/react';
 
 const Page = () => {
   return (
     <Container>
-      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+      <Box
+        borderRadius="lg"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        p={3}
+        mb={6}
+        align="center"
+      >
         Hello, I'm a full-stack developer based in Vancouver.
       </Box>
       <Box display={{ md: 'flex' }}>
@@ -12,6 +24,23 @@ const Page = () => {
             Nick Liu
           </Heading>
           <p> Fullstack Developer / Bodybuilder / Reader </p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/nick_round.png"
+            alt="Profile Image"
+          />
         </Box>
       </Box>
     </Container>
