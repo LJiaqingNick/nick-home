bab18f89170fc51048af0ed7c4242f9ae19e0248
LJiaqingNick <gmziue@gmail.com>
2024-02-11 10:48:38 -0800

    add toggle theme button

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,5 @@
 import { forwardRef } from 'react';
-import Logo from './logo';
+import Logo from './logo.js';
 import NextLink from 'next/link';
 import {
   Container,
@@ -13,9 +13,10 @@ import {
   MenuList,
   MenuButton,
   IconButton,
-  useColorModeValue
+  useColorModeValue,
 } from '@chakra-ui/react';
 import { HamburgerIcon } from '@chakra-ui/icons';
+import ThemeToggleButton from './teme-toggle-button';
 
 const LinkItem = ({ href, path, children }) => {
   const active = path === href;
@@ -79,6 +80,7 @@ const Navbar = props => {
           </LinkItem>
         </Stack>
         <Box flex={1} align="right">
+          <ThemeToggleButton />
           <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
             <Menu>
               <MenuButton
