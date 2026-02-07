50c15cc468dc8e2a0a504ff405000fdc78381194
LJiaqingNick <gmziue@gmail.com>
2026-02-07 14:22:05 -0800

    Remove attribution sentence from footer

--- components/footer.js
+++ components/footer.js
@@ -4,15 +4,6 @@ const Footer = () => {
   return (
     <Box align="center" opacity={0.4} fontSize="sm">
       &copy; {new Date().getFullYear()} Nick Liu. All Rights Reserved. <br />
-      This website is built based on the{' '}
-      <a
-        href="https://www.craftz.dog/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Takuya Matsuyama&apos;s website
-      </a>
-      .
     </Box>
   );
 };
